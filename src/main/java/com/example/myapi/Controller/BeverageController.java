639f95fb7d64e6b4b3195d9445e47c53f84adb0f
cgk95 <kjkj5868@gmail.com>
2023-04-27 02:47:18 +0900

    feat : 음료 등록 기능 구현, Optional을 활용할 수 있도록 getBeverageByName 메서드 변경

--- src/main/java/com/example/myapi/Controller/BeverageController.java
+++ src/main/java/com/example/myapi/Controller/BeverageController.java
@@ -5,9 +5,7 @@ import com.example.myapi.Service.BeverageService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -21,17 +19,24 @@ public class BeverageController {
         this.beverageService = beverageService;
     }
 
+    @GetMapping("/all")
     public List<Beverage> getAllBeverage() {
         return beverageService.getAllBeverages();
     }
 
     @GetMapping("/{name}")
     public ResponseEntity<Beverage> getBeverageByName(@PathVariable String name) {
-        Beverage beverage = beverageService.findBeverageByName(name);
+        Beverage beverage = beverageService.findBeverageByName(name).orElseThrow(() -> new IllegalArgumentException("존재하지 않는 음료 이름입니다."));
         if (beverage == null) {
             return ResponseEntity.notFound().build();
         }
         return ResponseEntity.ok().body(beverage);
+    }
+
+    @PostMapping
+    public ResponseEntity<BeverageDTO> addBeverage(@RequestBody BeverageDTO beverageDTO) { // HTTP 메서드의 BODY 로 SON 입력을 받아서 서비스로 넘겨주자
+        beverageService.addBeverage(beverageDTO);
+        return ResponseEntity.ok().build();
     }
 
 
