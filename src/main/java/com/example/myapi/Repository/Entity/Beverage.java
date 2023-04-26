fd6bd335de6c9c7e9c482f17d0801267b628cc9a
cgk95 <kjkj5868@gmail.com>
2023-04-26 15:41:27 +0900

    style : 불필요한 주석 삭제

--- src/main/java/com/example/myapi/Repository/Entity/Beverage.java
+++ src/main/java/com/example/myapi/Repository/Entity/Beverage.java
@@ -10,29 +10,22 @@ import java.util.List;
 
 
 @Entity
+@Repository
 @NoArgsConstructor
 @Getter
-@Repository
 @Table(name = "beverage") // 가장 하위의 아이템(음료) 정보를 담음
 public class Beverage {
-    /**
-     * 음료를 표현할 기본 키 :: 자동으로 증가
-     */
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "beverage_id")
     private Long id;
 
-    /**
-     * 음료 이름
-     */
     @Column(name = "name", nullable = false)
     private String name;
 
-    /**
-     * 음료에 대한 간단한 설명
-     */
     @Column(name = "description", nullable = false)
+
     private String description;
 
     @OneToMany(mappedBy = "beverage", cascade = CascadeType.ALL)
