85dd916a050c74643c9f03e1438ef20953c47274
cgk95 <kjkj5868@gmail.com>
2023-04-26 16:55:56 +0900

    feat : 모든 음료 조회 기능, 음료 이름으로 조회 기능 1차 구현

--- src/main/java/com/example/myapi/Controller/BeverageController.java
+++ src/main/java/com/example/myapi/Controller/BeverageController.java
@@ -1,14 +1,38 @@
 package com.example.myapi.Controller;
 
+import com.example.myapi.Repository.Entity.Beverage;
 import com.example.myapi.Service.BeverageService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
+@RequestMapping("/beverage")
 public class BeverageController {
-    private final BeverageService beverageService; // 다른 서비스 영역으로 변경될 일이 없기 때문에 final 처리.
+    private final BeverageService beverageService;
+
     @Autowired
     public BeverageController(BeverageService beverageService) {
-        this.beverageService  = beverageService;
+        this.beverageService = beverageService;
     }
+
+    public List<Beverage> getAllBeverage() {
+        return beverageService.getAllBeverages();
+    }
+
+    @GetMapping("/{name}")
+    public ResponseEntity<Beverage> getBeverageByName(@PathVariable String name) {
+        Beverage beverage = beverageService.findBeverageByName(name);
+        if (beverage == null) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok().body(beverage);
+    }
+
+
 }
--- src/main/java/com/example/myapi/Service/BeverageService.java
+++ src/main/java/com/example/myapi/Service/BeverageService.java
@@ -1,15 +1,26 @@
 package com.example.myapi.Service;
 
+import com.example.myapi.Repository.BeverageRepository;
 import com.example.myapi.Repository.Entity.Beverage;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class BeverageService {
-    private final Beverage beverage;
+    private final BeverageRepository beverageRepository;
+
     @Autowired
-    public BeverageService(Beverage beverage) {
-        this.beverage = beverage;
+    public BeverageService(BeverageRepository beverageRepository) {
+        this.beverageRepository = beverageRepository;
     }
 
+    public List<Beverage> getAllBeverages() {
+        return beverageRepository.findAll();
+    }
+
+    public Beverage findBeverageByName(String name) {
+        return this.beverageRepository.findBeverageByName(name);
+    }
 }
